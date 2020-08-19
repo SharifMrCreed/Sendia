72b639767e0e235a22ee592465e027ef36e1e5a7
MrCreed <shinshray@gmail.com>
2020-08-19 03:34:00 +0300

    Refubished the Profile page

--- app/src/main/java/com/alle/san/sendia/LogInFragment.java
+++ app/src/main/java/com/alle/san/sendia/LogInFragment.java
@@ -8,15 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alle.san.sendia.models.User;
 import com.alle.san.sendia.utils.Constants;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.Objects;
 
 
 public class LogInFragment extends Fragment {
 
     User user = new User();
+    ImageView backArrow;
+    ImageView photo;
+    TextView gender;
+    TextView name;
+    TextView interestedIn;
+    TextView status;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +46,30 @@ public class LogInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_log_in, container, false);
+
+        backArrow = view.findViewById(R.id.image_back_arrow);
+        photo = view.findViewById(R.id.profile_image);
+        name = view.findViewById(R.id.name);
+        gender = view.findViewById(R.id.gender);
+        interestedIn = view.findViewById(R.id.interested_in);
+        status = view.findViewById(R.id.status);
+
+        bind();
         return view;
     }
+
+    private void bind() {
+        RequestOptions requestOptions = new RequestOptions()
+                                            .placeholder(R.drawable.allecon);
+        Glide.with(getActivity())
+                .load(user.getProfile_image())
+                .apply(requestOptions)
+                .into(photo);
+        name.setText(user.getName());
+        gender.setText(user.getGender());
+        interestedIn.setText(user.getInterested_in());
+        status.setText(user.getStatus());
+    }
+
+
 }
